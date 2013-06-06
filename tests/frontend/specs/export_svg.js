a77564ae0d712821d0191a41381afdb24d35b5cf
tranek <tranekmobile@gmail.com>
2013-06-06 18:22:24 -0400

    Updated export SVG test spec for xlink xml namespace

--- tests/frontend/specs/export_svg.js
+++ tests/frontend/specs/export_svg.js
@@ -39,6 +39,7 @@ describe("Export SVG", function(){
     var svg = window.frames[0].paper.project.exportSVG();
     svg.setAttribute('version', '1.1');
     svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    svg.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
 
     var dummy = document.createElement('div');
     dummy.appendChild(svg);
