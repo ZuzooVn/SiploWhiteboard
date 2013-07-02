c4ace181043de40f01821d49d8fd3d81e152a893
John McLear <john@mclear.co.uk>
2013-07-03 00:18:07 +0100

    debug into test JSON file

--- bin/testJSON.js
+++ bin/testJSON.js
@@ -11,41 +11,37 @@ db.init(function (err) {
   if(err) {
     console.error(err);
   }
-  db.findKeys("*", "", function(err, rooms){
+  db.findKeys("*", null, function(err, rooms){
     var length = rooms.length;
+    console.log("Analyzing JSON for ", length, " rooms");
     // begin for each
     for (var i = 0; i < length; i++){
       var room = rooms[i];
       projects[room] = {};
       projects[room].project = new paper.Project(paper.projects[0].view);
       projects[room].external_paths = {};
+      console.log("attempting to load room # ", i);
       loadFromDB(room);
     }
   });
 });
 
+
 // Try to load room from database
 function loadFromDB(room) {
   if (projects[room] && projects[room].project) {
     var project = projects[room].project;
-    db.init(function (err) {
-      if(err) {
-        console.error(err);
+    db.get(room, function(err, value) {
+      if (value && project && project instanceof paper.Project && project.activeLayer) {
+        // Clear default layer as importing JSON adds a new layer.
+        // We want the project to always only have one layer.
+        project.activeLayer.remove();
+        project.importJSON(value.project);
+        console.log("Loaded room ", room);
+        projects[room].destroy; // remove it
+        console.log("Destroyed room", room);
       }
-      db.get(room, function(err, value) {
-        if (value && project && project instanceof paper.Project && project.activeLayer) {
-          // Clear default layer as importing JSON adds a new layer.
-          // We want the project to always only have one layer.
-          project.activeLayer.remove();
-          project.importJSON(value.project);
-          console.log("Loaded room", room);
-          projects[room].destroy; // remove it
-          console.log("Destroyed room", room);
-        }
-        db.close(function(){});
-      });
     });
-  } else {
-    // loadError(socket);
   }
 }
+
