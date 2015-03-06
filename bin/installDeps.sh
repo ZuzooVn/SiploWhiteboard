7e8a64860c0446464f57232c14749a941bcd4f79
John McLear <john@mclear.co.uk>
2015-03-06 12:49:50 +0000

    work on node 12

--- bin/installDeps.sh
+++ bin/installDeps.sh
@@ -34,21 +34,6 @@ hash npm > /dev/null 2>&1 || {
   exit 1 
 }
 
-#check npm version
-NPM_VERSION=$(npm --version)
-if [ ! $(echo $NPM_VERSION | cut -d "." -f 1) = "1" ]; then
-  echo "You're running a wrong version of npm, you're using $NPM_VERSION, we need 1.x" >&2
-  exit 1 
-fi
-
-#check node version
-#NODE_VERSION=$(node --version)
-#NODE_V_MINOR=$(echo $NODE_VERSION | cut -d "." -f 1-2)
-#if [ ! $NODE_V_MINOR = "v0.8" ] && [ ! $NODE_V_MINOR = "v0.10" ]; then
-#  echo "You're running a wrong version of node, you're using $NODE_VERSION, we need v0.8.x or v0.10.x" >&2
-#  exit 1
-#fi
-
 #Get the name of the settings file
 settings="settings.json"
 a='';
