e3f9a6752b66e299de9f4f230b5030a5a2eb7761
John McLear <john@mclear.co.uk>
2015-04-29 20:23:05 +0100

    begin adding settings for brush and clean up

--- src/util/Settings.js
+++ src/util/Settings.js
@@ -19,42 +19,45 @@ exports.defaults = {
   "dbSettings" : {
                    "filename" : "var/dirty.db"
                  },
-  "ssl": false
+  "ssl": false,
+  "tool": "brush",
 };
 
 
 
 exports.loadSettings = function() {
-	var settings_file = "settings.json";
-	var user_settings = {};
-	try {
-		  user_settings = fs.readFileSync(settings_file).toString();
-		  //minify to remove comments and whitepsace before parsing
-		  user_settings = JSON.parse(JSON.minify(user_settings));
-	}
-	catch(e){
-		console.error('There was an error processing your settings.json file: '+e.message);
-		process.exit(1);
-	}
+  var settings_file = "settings.json";
+  var user_settings = {};
+  try {
+    user_settings = fs.readFileSync(settings_file).toString();
+    //minify to remove comments and whitepsace before parsing
+    user_settings = JSON.parse(JSON.minify(user_settings));
+  }
+  catch(e){
+    console.error('There was an error processing your settings.json file: '+e.message);
+    process.exit(1);
+  }
 	
-    //copy over defaults
-	for(var k in exports.defaults){exports[k] = exports.defaults[k]}
-	//go through each key in the user supplied settings and replace the defaults
-	//if a key is not in the defaults, warn the user and ignore it
-	for(var k in user_settings) {
-		if(k in exports.defaults){
-            //overwrite it
-            exports[k] = user_settings[k];
-        }
-        else
-        {
-            console.warn("'Unknown Setting: '" + k + "'. This setting doesn't exist or it was removed");
-        }
-	}
-    //settings specific warnings
-    if(exports.dbType === "dirty"){
-        console.warn("DirtyDB is used. This is fine for testing but not recommended for production.");
+  //copy over defaults
+  for(var k in exports.defaults){
+    exports[k] = exports.defaults[k]
+  }
+
+  //go through each key in the user supplied settings and replace the defaults
+  //if a key is not in the defaults, warn the user and ignore it
+  for(var k in user_settings) {
+    if(k in exports.defaults){
+      //overwrite it
+      exports[k] = user_settings[k];
+    }else{
+      console.warn("'Unknown Setting: '" + k + "'. This setting doesn't exist or it was removed");
     }
+  }
+
+  //settings specific warnings
+  if(exports.dbType === "dirty"){
+    console.warn("DirtyDB is used. This is fine for testing but not recommended for production.");
+  }
 
 };
 
