c856475df3e166d8af3e19935d382068ae8cb50d
John McLear <john@mclear.co.uk>
2015-04-29 21:44:59 +0100

    more database logic elsewhere and update example to use proper db

--- src/util/db.js
+++ src/util/db.js
@@ -5,17 +5,19 @@ var settings = require('./Settings.js'),
 // Database connection
 var db = new ueberDB.database(settings.dbType, settings.dbSettings);
 
+// Init..
+db.init(function(err){
+  if(err){
+    console.error(err);
+  }
+});
+
 // Write to teh database
 exports.storeProject = function(room) {
   var project = projects.projects[room].project;
   var json = project.exportJSON();
-  db.init(function (err) {
-    if(err) {
-      console.error(err);
-    }
-    console.log("Writing project to database");
-    db.set(room, {project: json});
-  });
+  console.log("Writing project to database");
+  db.set(room, {project: json});
 }
 
 // Try to load room from database
@@ -23,28 +25,21 @@ exports.load = function(room, socket) {
   console.log("load from db");
   if (projects.projects[room] && projects.projects[room].project) {
     var project = projects.projects[room].project;
-    db.init(function (err) {
-      if(err) {
-        console.error(err);
+    db.get(room, function(err, value) {
+      if (value && project && project.activeLayer) {
+        socket.emit('loading:start');
+        // Clear default layer as importing JSON adds a new layer.
+        // We want the project to always only have one layer.
+        project.activeLayer.remove();
+        project.importJSON(value.project);
+        socket.emit('project:load', value);
       }
-      console.log("Initting db");
-      db.get(room, function(err, value) {
-//        if (value && project && project instanceof drawing.Project && project.activeLayer) {
-        if (value && project && project.activeLayer) {
-          socket.emit('loading:start');
-          // Clear default layer as importing JSON adds a new layer.
-          // We want the project to always only have one layer.
-          project.activeLayer.remove();
-          project.importJSON(value.project);
-          socket.emit('project:load', value);
-        }
-        socket.emit('loading:end');
-        db.close(function(){});
-      });
-      socket.emit('loading:end'); // used for sending back a blank database in case we try to load from DB but no project exists
+      socket.emit('loading:end');
     });
+    socket.emit('loading:end'); // used for sending back a blank database in case we try to load from DB but no project exists
   } else {
     loadError(socket);
   }
 }
 
+exports.db = db;
