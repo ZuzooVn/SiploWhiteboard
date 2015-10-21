d142dd02166e720a6e6fdd91ea1a0d73314249b4
buddhikajay <buddhika.12@cse.mrt.ac.lk>
2015-10-21 23:55:15 +0530

    changed root start sentence error

--- bin/run.sh
+++ bin/run.sh
@@ -13,7 +13,7 @@ if [ "$(id -u)" -eq 0 ]; then
    echo "You shouldn't start EtherDraw as root!"
    echo "Please type 'EtherDraw' if you still want to start it as root"
    read rocks
-   if [ ! $rocks = "EtherDraw rocks my socks" ]
+   if [ ! $rocks = "EtherDraw" ]
    then
      echo "Your input was incorrect"
      exit 1
