877ff304e6a4e357fc5b1cf7d17706c2124fc5ac
buddhikajay <buddhika.12@cse.mrt.ac.lk>
2016-05-25 15:29:06 +0530

    modal body is scrollable & open button is working

--- src/static/pdfjs/fileBrowser/siploPdfViewer.js
+++ src/static/pdfjs/fileBrowser/siploPdfViewer.js
@@ -14,3 +14,26 @@ $(function() {
         }
     });
 });
+
+$(function(){
+    $('#container').on("changed.jstree", function (e, data) {
+        //console.log(data.instance.get_selected(true)[0].text);
+        //console.log(data.instance.get_node(data.selected[0]).li_attr.isLeaf);
+
+        //if the selected node is a leaf node -> enable the open button
+        var openFileButton = $('#openFileButton')
+        if(data.instance.get_node(data.selected[0]).li_attr.isLeaf){
+            openFileButton.prop('disabled', false);
+            openFileButton.click(function(){
+                console.log('openning ' + data.instance.get_selected(true)[0].text);
+            });
+        }
+        else {
+            $('#openFileButton').prop('disabled', true);
+        }
+    });
+});
+
+function openFle(){
+
+}
