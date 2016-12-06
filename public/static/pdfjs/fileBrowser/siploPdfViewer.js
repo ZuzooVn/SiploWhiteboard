894e4755efa9f8e9bb593de3d8db6f4056ad79d9
AselaDarshan <aseladarshan@gmail.com>
2016-12-06 23:12:26 +0530

    Fix issue in file location url

--- public/static/pdfjs/fileBrowser/siploPdfViewer.js
+++ public/static/pdfjs/fileBrowser/siploPdfViewer.js
@@ -120,7 +120,7 @@ function setupPDFRendering(file, callback){
 
     // If absolute URL from the remote server is provided, configure the CORS
     // header on that server.
-    var url = location.protocol+"//files."+location.host+"/api"+parentDirectory+"/"+file;
+    var url = location.protocol+"//files."+location.host+"/api/"+parentDirectory+"/"+file;
     // var url = location.protocol+"//localhost:5000/api/"+parentDirectory+"/"+file;
 
     pdfDoc = null;
