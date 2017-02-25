05e90af2fa3abc3737b1b953c4655777d0afe45f
AselaDarshan <aseladarshan@gmail.com>
2017-02-25 10:38:58 +0530

    Allow All Origins in tree

--- whiteboard.js
+++ whiteboard.js
@@ -153,7 +153,7 @@ app.get('/tree', function(req, res){
   console.log(req.query.room);
   var batchCode = '12';  // later get the batch n module-code from DB
   var moduleCode = 'CS2036';
-  res.header("Access-Control-Allow-Origin", "https://whiteboard.siplo.lk");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   files.processPath(req, res, batchCode, moduleCode);
 });
